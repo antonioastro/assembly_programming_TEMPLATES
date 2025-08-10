850bc7e5b2df10cab1250ce1fb2b416485b33f63
acelectronics <antonioastro@proton.me>
2025-08-10 17:11:08 +0100

    Add files via upload

---
+++ PIC12f510_template.s
@@ -0,0 +1,88 @@
+;Template created by ANTONIO COULTON
+;Email: acelectronics@murena.io
+;Last updated August 2025
+    
+PROCESSOR 12F510 ;tell the software which chip is used.
+   
+  CONFIG  OSC = IntRC           ; Oscillator Select (INTOSC with 1.125 ms DRT)
+  CONFIG  WDT = OFF             ; Watchdog Timer Enable bit (WDT disabled)
+  CONFIG  CP = OFF              ; Code Protect (Code protection off)
+  CONFIG  MCLRE = OFF           ; Master Clear Enable bit (GP3/MCLR pin functions as GP3, MCLR internally tied to VDD)
+  CONFIG  IOSCFS = OFF          ; Internal Oscillator Frequency Select bit (4 MHz INTOSC Speed)
+    
+#include <xc.inc>
+  
+WAIT1	EQU 0x21	;values used to refer to memory stores used in wait delays
+WAIT10	EQU 0x22	
+WAIT100	EQU 0x23	
+WAIT1k	EQU 0x24
+	
+;variables used to point to memory stores for the readadc routines
+ADC0	EQU 0x32
+	
+;===================================== SUBROUTINES ========================================
+psect code
+
+wait1ms:    ;@4MHz, the exe rate = 1MIP, t=1us. 1000 total instructions needed to make 1ms.
+    movlw 255	    ;+1 instruction
+    movwf WAIT1	    ;+1
+loop1ms:
+	decfsz WAIT1,1	;+1*255
+	goto loop1ms	;+2(255-1)
+	movlw 77		;+1
+	movwf WAIT1		;+1
+loop1ms2:
+	decfsz WAIT1,1	;+1*77
+	goto loop1ms2	;+2(77-1)
+	nop		;+1
+	nop		;+1
+	return	;+2 = 1000 total instructions
+
+wait10ms: ;requires 10,000 instructions
+	movlw 10	;+1
+	movwf WAIT10	;+1
+loop10ms:
+	call wait1ms ;10*1000 instructions
+	decfsz WAIT10,1 ;+10
+	goto loop10ms ;+2*9
+	return ;+2 = 10,030 total
+
+wait100ms: ;requires 100,000 instructions
+	movlw 100	    ;+1
+	movwf WAIT100	    ;+1
+loop100ms:
+	call wait1ms	    ;+100*1000
+	decfsz WAIT100,1    ;+100
+	goto loop100ms	    ;+99*2
+	return		    ;+2 = 100,302
+
+wait1000ms:		;requires 1,000,000 instructions
+	movlw 10	;+1
+	movwf WAIT1k	;+1
+loop1000ms:
+	call wait100ms	;+10*100,302
+	decfsz WAIT1k,1	;+10
+	goto loop1000ms	;+2*9
+	return		;+2 = 1,003,052
+
+readadc2:
+	bsf ADCON0,6	;bits 6 and 7 inform the microcontroller which AN pin to check.
+	bcf ADCON0,7	;this configuration enables AN2/GP2
+	bsf ADCON0,1	;start adc conversion
+loopadc2:
+	clrwdt		    ;Pat the watchdog
+	btfsc ADCON0,1	    ;check if conversion finished
+	goto loopadc0
+	movf ADRES,0x00	    ;take result from ADRESH
+	movwf ADC0	    ;move result to ADC0
+	return
+
+psect RES_VECT,class=CODE,delta=2 ; PIC10/12/16. "-PRES_VECT=0x00" must be present as a custom linker option in the project properties.
+RES_VECT:
+    movlw 001111	
+    movwf TRISIO	;GP3 must be INPUT.
+    bcf ADCON0,0	;adc OFF by default.
+
+main:
+    nop ;replace with your program
+END
